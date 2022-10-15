545ec805065bdaa8dde74af25e6606f8204ae8f9
999-4L <112986907+999-4L@users.noreply.github.com>
2022-10-15 18:10:33 +0530

    Update login.js

--- login.js
+++ login.js
@@ -8,12 +8,3 @@ profileName.addEventListener("keyup", () => {
     submitBtn.setAttribute("disabled", "");
   }
 });
-
-submitBtn.addEventListener("click", () => {
-  document.cookie = "username" + "=" + profileName.value;
-  "SameSite = Lax";
-  console.log(document.cookie);
-});
-console.log(document.cookie);
-document.cookie = "username" + "=;expires=Thu, 01 Jan 1970 00:00:01 GMT; SameSite = Lax";
-console.log(document.cookie);
